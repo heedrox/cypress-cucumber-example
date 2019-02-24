96a9f5e4db85e764fac36a43e38ffb57d3b1baa2
jmarti <jmarti@theinit.com>
2019-02-24 10:23:59 +0100

    pull features

--- cypress/pages/browser.js
+++ cypress/pages/browser.js
@@ -1,4 +1,7 @@
 class Browser {
+  // You can use Browser class to do some typical things, like
+  // login, etc.
+
   static expect() {
     return {
       toBeInOtherDomain: () => cy.url().should('not.include', 'google'),
